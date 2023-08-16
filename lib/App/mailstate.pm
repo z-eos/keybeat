39e90c6adbc01c5d8154836c747f3996c05d3bb2
Zeus Panchenko <zeus@gnu.org.ua>
2023-08-16 17:40:40 +0300

    refactored as module, initial changes

--- lib/App/mailstate.pm
+++ lib/App/mailstate.pm
@@ -8,6 +8,7 @@ use strict;
 use warnings;
 use diagnostics;
 
+use DBI;
 use Data::Printer caller_info => 1, colored => 1, print_escapes => 1, output => 'stdout', class => { expand => 2 },
   caller_message => "DEBUG __FILENAME__:__LINE__ ";
 use File::Basename;
@@ -17,6 +18,7 @@ use Net::LDAP;
 use Parse::Syslog::Mail;
 use Pod::Man;
 use Pod::Usage qw(pod2usage);;
+use Sys::Syslog qw(:standard :extended :macros);
 use Time::Piece;
 
 my  @PROGARG = ($0, @ARGV);
@@ -30,22 +32,22 @@ sub new {
 	   _progargs => [$0, @ARGV],
 	   _option   => { d                 => 0,
 			  colored           => 0,
-			  logfile           => '/var/log/maillog',
+			  log => {
+				  logfile => '/var/log/maillog',
+				  save_to => './',
+				 },
 			  relay_domains_sfx => [ 'root','xx','ibs','ibs.dn.ua' ],
-			  verbose           => 0,
+			  v                 => 0,
 			  dryrun            => 0,
 			  count             => 1,
-			  export            => '',
-			  tl                => localtime,
-			  
+			  export            => 'raw',
 			},
 	  }, $class;
 
   GetOptions(
-             'l|logfile=s' => \$self->{_option}{logfile},
-             's|save-to=s' => \$self->{_option}{ log->{saveto} },
-	     'h|help'      => sub { pod2usage(0); exit 0 },
-	     'v+'          => \$self->{_option}{verbose},
+             'l|logfile=s' => \$self->{_option}{log}{logfile},
+             's|save-to=s' => \$self->{_option}{log}{save_to},
+	     'v+'          => \$self->{_option}{v},
 	     'c'           => \$self->{_option}{count},
 	     'e|export=s'  => \$self->{_option}{export},
 	     'n|dry-run'   => \$self->{_option}{dryrun},
@@ -55,24 +57,33 @@ sub new {
 	     'V|version'           => sub { print "$self->{_progname}, version $VERSION\n"; exit 0 },
 	    );
 
-  pod2usage(-exitval => 0, -verbose => 2, -msg => "\nERROR: Wrong export format.\n\n")
-    if $self->{_option}{export} && 
+  if ( $self->{_option}{export} && 
        $self->{_option}{export} ne 'sqlite' &&
-       $self->{_option}{export} ne 'raw';
+       $self->{_option}{export} ne 'raw' ) {
+    $self->debug_msg( {priority => 'err',
+		message   => "error: Wrong export format. Allowed formats are sqlite and raw",
+		verbosity => $self->{_option}{v} });
+    exit 1;
+  }
 
-  print "log file to be used is: $logfile\n" if $verbose > 0;
+  print "log file to be used is: $self->{_option}{log}{logfile}\n" if $self->{_option}{v} > 0;
 
-  pod2usage(-exitval => 0, -verbose => 2, -msg => "\nERROR: log file configured is $logfile; %m\n\n")
-    if ! -f $self->{_option}{logfile};
-
-  pod2usage(-exitval => 0, -verbose => 2, -msg => "\nERROR: no extension given, set it please.\n\n")
-    if ! $self->{_option}{export};
-
-  pod2usage(-exitval => 0, -verbose => 2, -msg => "\nERROR: no extension given, set it please.\n\n")
-    if ( defined $log->{saveto} && ! -d $log->{saveto} ) {
-    debug_msg( {priority => 'warning',
-		message   => "warning: directory to save db file to is $log->{saveto}; %m",
-		verbosity => $verbose });
+  if ( ! -f $self->{_option}{log}{logfile} ) {
+    $self->debug_msg({ priority => 'err',
+		message   => "error: log file configured is $self->{_option}{log}{logfile}; %m",
+		verbosity => $self->{_option}{v} });
+    # pod2usage(0);
+    exit 1;
+  } elsif ( ! $self->{_option}{export} ) {
+    $self->debug_msg({ priority => 'warning',
+		message   => "warning: no extension given, set it please",
+		verbosity => $self->{_option}{v} });
+    # pod2usage(0);
+    exit 1;
+  } elsif ( defined $self->{log}->{save_to} && ! -d $self->{log}->{save_to} ) {
+    $self->debug_msg( {priority => 'warning',
+		message   => "warning: directory to save db file to is $self->{log}->{save_to}; %m",
+		verbosity => $self->{_option}{v} });
     # pod2usage(0);
     exit 1;
   }
@@ -83,91 +94,95 @@ sub new {
 sub progname { shift->{_progname} }
 sub progargs { return join(' ', @{shift->{_progargs}}); }
 
-sub option {
+sub o {
   my ($self,$opt) = @_;
   return $self->{_option}{$opt};
 }
 
-# my $progname = 'mailstate';
-# my $logfile = '/var/log/maillog';
-# my $relay_domains_sfx = [ 'root','xx','ibs','ibs.dn.ua' ];
-# our $verbose = 0;
-# our $dryrun = 0;
-# my $count = 1;
-# my $export = '';
-# my $tl = localtime;
+sub l { shift->{_option}{log} }
 
-my $log;
-my @log_row;
-my $res;
-my $index;
-my $element;
-my $id;
-my $ts;
-my $t;
-my $rest;
-my $key;
-my $val;
-my $i;
+sub v { shift->{_option}{v} }
 
-$log->{saveto} = '';
+sub run {
+  my $self = shift;
 
-  
-$log->{logfile} = $logfile;
-( $log->{name}, $log->{dirs}, $log->{suffix} ) = fileparse($logfile);
-$log->{stat} = stat($logfile);
+  my $element;
+  my $i;
+  my $id;
+  my $index;
+  my $key;
+  my $log;
+  my $res;
+  my $rest;
+  my $t;
+  my $ts;
+  my $val;
+  my @log_row;
 
-my $maillog = Parse::Syslog::Mail->new( $logfile,
-				        allow_future => 1);
-while( my $row = $maillog->next ) {
-  next if $row->{text} =~ /AUTH|STARTTLS|--|NOQUEUE/;
+  ( $self->l->{name}, $self->l->{dirs}, $self->l->{suffix} ) = fileparse($self->o('log')->{logfile});
+  $self->l->{stat} = stat($self->o('log')->{logfile});
 
-  if ( exists $row->{'to'} ) {
+  my $maillog = Parse::Syslog::Mail->new( $self->o('log')->{logfile},
+					  allow_future => 1);
+  while ( my $r = $maillog->next ) {
+    next if $r->{text} =~ /AUTH|STARTTLS|--|NOQUEUE/;
 
-    $res->{$row->{id}}->{timestamp}->{to} = $row->{timestamp}         // 'NA';
-    $res->{$row->{id}}->{delay}           = $row->{delay}             // 'NA';
-    $res->{$row->{id}}->{xdelay}          = $row->{xdelay}            // 'NA';
-    $res->{$row->{id}}->{dsn}             = $row->{dsn}               // 'NA';
-    $res->{$row->{id}}->{status}          = $row->{status}            // 'NA';
-    $res->{$row->{id}}->{addr}->{to}      = strip_addr($row->{to})
-      if exists $row->{to};
-    $res->{$row->{id}}->{relay}->{to}     = split_relay($row->{relay})
-      if exists $row->{relay};
+    if ( exists $r->{'to'} ) {
 
-  } elsif ( exists $row->{'from'} ) {
+      $res->{$r->{id}}->{timestamp}->{to} = $r->{timestamp}         // 'NA';
+      $res->{$r->{id}}->{delay}           = $r->{delay}             // 'NA';
+      $res->{$r->{id}}->{xdelay}          = $r->{xdelay}            // 'NA';
+      $res->{$r->{id}}->{dsn}             = $r->{dsn}               // 'NA';
+      $res->{$r->{id}}->{status}          = $r->{status}            // 'NA';
+      $res->{$r->{id}}->{addr}->{to}      = $self->strip_addr($r->{to})
+	if exists $r->{to};
+      $res->{$r->{id}}->{relay}->{to}     = $self->split_relay($r->{relay})
+	if exists $r->{relay};
 
-    $res->{$row->{id}}->{timestamp}->{fr} = $row->{timestamp}         // 'NA';
-    $res->{$row->{id}}->{size}            = $row->{size};
-    $res->{$row->{id}}->{addr}->{fr}      = strip_addr($row->{from})
-      if exists $row->{from};
-    $res->{$row->{id}}->{msgid}           = strip_addr($row->{msgid})
-      if exists $row->{msgid};
-    $res->{$row->{id}}->{relay}->{fr}     = split_relay($row->{relay})
-      if exists $row->{relay};
+    } elsif ( exists $r->{'from'} ) {
+
+      $res->{$r->{id}}->{timestamp}->{fr} = $r->{timestamp}         // 'NA';
+      $res->{$r->{id}}->{size}            = $r->{size};
+      $res->{$r->{id}}->{addr}->{fr}      = $self->strip_addr($r->{from})
+	if exists $r->{from};
+      $res->{$r->{id}}->{msgid}           = $self->strip_addr($r->{msgid})
+	if exists $r->{msgid};
+      $res->{$r->{id}}->{relay}->{fr}     = $self->split_relay($r->{relay})
+	if exists $r->{relay};
+    }
+
+    $res->{$r->{id}}->{connection} = $r->{status}
+      if exists $r->{status} &&
+      $r->{status} =~ /^.*connection.*$/ &&
+      $r->{status} !~ /^.*did not issue.*$/;
   }
 
-  $res->{$row->{id}}->{connection} = $row->{status}
-    if exists $row->{status} &&
-    $row->{status} =~ /^.*connection.*$/ &&
-    $row->{status} !~ /^.*did not issue.*$/;
+  p $res if $self->o('export') eq 'raw' || $self->v > 2;
+
+  $self->tosqlite( { log_rows  => $res, } )
+    if $self->o('export') eq 'sqlite' && ! $self->o('dryrun');
+
+  exit 0;
 }
-
-p $res if $export eq 'raw' || $verbose > 2;
-
-tosqlite( { log_rows  => $res,
-	    log       => $log,
-	    localtime => $tl,
-	  } )
-  if $export eq 'sqlite' && ! $dryrun;
-
-exit 0;
 
 ######################################################################
 #
 ######################################################################
 
+sub debug_msg {
+  my ($self, $args) = @_;
+  my $arg = { priority  => $args->{priority},
+	      message   => $args->{message},
+	      verbosity => $args->{verbosity} || 0, };
+
+  openlog($self->{_progname}, "pid", LOG_MAIL);
+  syslog($arg->{priority}, $arg->{message});
+  closelog();
+  print "DEBUG: $arg->{message}\n" if $arg->{verbosity} > 0
+}
+
 sub split_relay {
-  my $relay = shift;
+  my ($self, $relay) = @_;
   my $return;
   if ( $relay =~ /^([\w,\.]+) \[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\].*$/ ) {
     $return->{fqdn} = $1;
@@ -186,7 +201,7 @@ sub split_relay {
 }
 
 sub strip_addr {
-  my $addr = shift;
+  my ($self, $addr) = @_;
   my $return;
   if ($addr =~ /<(.*@.*)>/) {
     $return = $1;
@@ -197,26 +212,27 @@ sub strip_addr {
 }
 
 sub tosqlite {
-  my $args = shift @_;
-  $args->{logfilemtime} = localtime($args->{log}->{stat}->mtime);
+  my ($self, $args) = @_;
 
-  my $arg = { log_rows  => $args->{log_rows},
-	      log       => $args->{log},
-	      localtime => $args->{localtime},
-	      dbfile    =>
-	      sprintf('%s%s-%s-v%s%s.sqlite',
-		      $args->{log}->{saveto} ne '' ? $args->{log}->{saveto} . '/' : $args->{log}->{dirs},
-		      $args->{log}->{name},
-		      $args->{logfilemtime}->ymd(''),
-		      $args->{localtime}->ymd(''),
-		      $args->{localtime}->hms(''),
-		     ),
-	      verbose => $args->{verbose}, };
+  $args->{logfilemtime} = localtime($self->l->{stat}->mtime);
 
-  p $arg if $verbose > 3;
-  print "database file to be used is: $arg->{dbfile}\n" if $verbose;
+  my $tl = localtime;
+  my $arg =
+    {
+     log_rows  => $args->{log_rows},
+     localtime => $self->{_tl},
+     dbfile    =>
+     sprintf('%s%s-%s-v%s%s.sqlite',
+	     $self->l->{save_to} ne '' ? $self->l->{save_to} . '/' : $self->l->{dirs},
+	     $self->l->{name},
+	     $args->{logfilemtime}->ymd(''),
+	     $tl->ymd(''),
+	     $tl->hms(''),
+	    ),
+    };
 
-  use DBI;
+  p $arg if $self->v > 3;
+  print "database file to be used is: $arg->{dbfile}\n" if $self->v;
 
   my $dbh = DBI->connect("dbi:SQLite:dbname=$arg->{dbfile}","","",
 			 { AutoCommit => 1,
@@ -226,7 +242,7 @@ sub tosqlite {
   $dbh->begin_work or die $dbh->errstr;
 
   my $tbl_create = qq{CREATE TABLE maillog
-  -- $arg->{log}->{logfile} data processed, generated with mailstate
+  -- $self->l->{logfile} data processed, generated with mailstate
   -- results are written to $arg->{dbfile}
   (
     id              TEXT PRIMARY KEY, -- sendmail message ID (macros \$i)
@@ -244,7 +260,7 @@ sub tosqlite {
     msgid           TEXT,             -- Message-ID header
     stat            TEXT              -- Status
   );};
-  p $tbl_create if $verbose > 1;
+  p $tbl_create if $self->v > 1;
   $dbh->do($tbl_create) or die $dbh->errstr;
 
   my $idx_create = [ q{CREATE INDEX addr_fr ON maillog ( addr_fr );},
@@ -258,7 +274,7 @@ sub tosqlite {
 		     q{CREATE INDEX stat ON maillog ( stat );} ];
 
   foreach (@{$idx_create}) {
-    p $_ if $verbose > 1;
+    p $_ if $self->v > 1;
     $dbh->do($_) or die $dbh->errstr;
   }
 
@@ -288,14 +304,14 @@ sub tosqlite {
 
   $dbh->begin_work or die $dbh->errstr;
   $tbl_create = qq{CREATE TABLE IF NOT EXISTS addr_to_unique
-  -- $arg->{log}->{logfile} data processed, generated with mailstate
+  -- $self->l->{logfile} data processed, generated with mailstate
   -- results are written to $arg->{dbfile}
   -- table contains all unique, not served by us, RCPT TO addresses 
   (
     addr_to_unique TEXT PRIMARY KEY, -- RCPT TO (not ours recipients)
     addr_to_count  NUM               -- emails sent to addr_to number
   );};
-  p $tbl_create if $verbose > 1;
+  p $tbl_create if $self->v > 1;
   $dbh->do($tbl_create) or die $dbh->errstr;
 
   $arg->{addr_to_unique_select} = sprintf("
@@ -303,9 +319,9 @@ INSERT INTO addr_to_unique (addr_to_unique, addr_to_count)
        SELECT DISTINCT lower(addr_to), count(addr_to)
               FROM maillog WHERE substr(addr_to,instr(addr_to, '\@') + 1) NOT IN ( '%s' )
               AND addr_to NOT LIKE '%%,%%' GROUP BY addr_to",
-					  join("','", @{relay_domains()}, @{$relay_domains_sfx}));
+					  join("','", @{relay_domains()}, @{$self->o('relay_domains_sfx')}));
 
-  p $arg->{addr_to_unique_select} if $verbose > 1;
+  p $arg->{addr_to_unique_select} if $self->v > 1;
   $dbh->do($arg->{addr_to_unique_select}) or die $dbh->errstr;
   #$sth = $dbh->prepare( $arg->{addr_to_unique_select} );
   #$sth->execute( "'" . join("','", @{relay_domains()}) . "','root'" );
@@ -313,47 +329,36 @@ INSERT INTO addr_to_unique (addr_to_unique, addr_to_count)
 
   $dbh->disconnect;
 
-  debug_msg({ priority  => 'info',
+  $self->debug_msg({ priority  => 'info',
 	      message   => sprintf('info: processing complete %s%s -> %s',
-				  $arg->{log}->{dirs},
-				  $arg->{log}->{name},
+				  $self->l->{dirs},
+				  $self->l->{name},
 				  $arg->{dbfile}),
-	      verbosity => $verbose });
+	      verbosity => $self->v });
 }
 
 sub relay_domains {
-  my $args = shift @_;
-  my $relay_domains;
-  my $ldap = Net::LDAP->new ( 'localhost', async => 1 ) or die "$@";
-  my $bind_msg = $ldap->bind ( version => 3 );
-  p $bind_msg->error if $bind_msg->code;
-  my $fqdns =
-    $ldap->search(base   => "sendmailMTAMapName=smarttable,ou=relay.xx,ou=Sendmail,dc=ibs",
-		  scope  => "one",
-		  filter => "sendmailMTAKey=*",
-		  attrs  => [ 'sendmailMTAKey' ],
-		 );
-  my @fqdn_arr = $fqdns->entries;
-  my $a;
-  foreach ( @fqdn_arr ) {
-    $a = $_->get_value('sendmailMTAKey', asref => 1);
-    # p $a;
-    push @{$relay_domains}, substr($a->[0],1);
-  }
-  return $relay_domains;
-}
-  
-sub debug_msg {
-  my $args = shift @_;
-  my $arg = { priority => $args->{priority},
-	      message => $args->{message},
-	      verbosity => $args->{verbosity} || 0, };
-
-  use Sys::Syslog qw(:standard :extended :macros);
-  openlog($progname, "pid", LOG_MAIL);
-  syslog($arg->{priority}, $arg->{message});
-  closelog();
-  print "DEBUG: $arg->{message}\n" if $arg->{verbosity} > 0
+  my ($self, $args) = @_;
+  #   my $args = shift @_;
+#   my $relay_domains;
+#   my $ldap = Net::LDAP->new ( 'localhost', async => 1 ) or die "$@";
+#   my $bind_msg = $ldap->bind ( version => 3 );
+#   p $bind_msg->error if $bind_msg->code;
+#   my $fqdns =
+#     $ldap->search(base   => "sendmailMTAMapName=smarttable,ou=relay.xx,ou=Sendmail,dc=ibs",
+# 		  scope  => "one",
+# 		  filter => "sendmailMTAKey=*",
+# 		  attrs  => [ 'sendmailMTAKey' ],
+# 		 );
+#   my @fqdn_arr = $fqdns->entries;
+#   my $a;
+#   foreach ( @fqdn_arr ) {
+#     $a = $_->get_value('sendmailMTAKey', asref => 1);
+#     # p $a;
+#     push @{$relay_domains}, substr($a->[0],1);
+#   }
+#   return $relay_domains;
+  return [];
 }
 
 1;
