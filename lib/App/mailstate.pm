40992d8cc2effe14a0b8f542a8c2bd692502a46d
Zeus Panchenko <zeus@gnu.org.ua>
2023-08-18 00:09:55 +0300

    sql methods added

--- lib/App/mailstate.pm
+++ lib/App/mailstate.pm
@@ -13,6 +13,7 @@ use Data::Printer caller_info => 1, colored => 1, print_escapes => 1, output => 
   caller_message => "DEBUG __FILENAME__:__LINE__ ";
 use File::Basename;
 use File::stat;
+use File::Tail;
 use Getopt::Long  qw(:config no_ignore_case gnu_getopt auto_help auto_version);
 use Net::LDAP;
 use Parse::Syslog::Mail;
@@ -34,13 +35,14 @@ sub new {
 			  colored           => 0,
 			  log => {
 				  logfile => '/var/log/maillog',
-				  save_to => './',
+				  save_to => '',
 				 },
 			  relay_domains_sfx => [ 'root','xx','ibs','ibs.dn.ua' ],
 			  v                 => 0,
 			  dryrun            => 0,
 			  count             => 1,
 			  export            => 'raw',
+			  tail              => 0,
 			},
 	  }, $class;
 
@@ -51,6 +53,7 @@ sub new {
 	     'c'           => \$self->{_option}{count},
 	     'e|export=s'  => \$self->{_option}{export},
 	     'n|dry-run'   => \$self->{_option}{dryrun},
+	     't|tail'      => \$self->{_option}{tail},
 
 	     'h|help'              => sub { pod2usage(-exitval => 0, -verbose => 2); exit 0 },
 	     'd|debug+'            => \$self->{_option}{d},
@@ -122,8 +125,21 @@ sub run {
   ( $self->l->{name}, $self->l->{dirs}, $self->l->{suffix} ) = fileparse($self->o('log')->{logfile});
   $self->l->{stat} = stat($self->o('log')->{logfile});
 
-  my $maillog = Parse::Syslog::Mail->new( $self->o('log')->{logfile},
-					  allow_future => 1);
+  my $file;
+  if ( $self->o('tail') ) {
+    $file = File::Tail->new($self->o('log')->{logfile});
+  } else {
+    $file = $self->o('log')->{logfile};
+  }
+  my $maillog = Parse::Syslog::Mail->
+    new( $self->o('tail') ? File::Tail->new($self->o('log')->{logfile}) : 
+	 $self->o('log')->{logfile},
+	 allow_future => 1);
+
+  $self->sql_db_create
+    if $self->o('export') eq 'sqlite' &&
+    ! exists $self->l->{db}->{name};
+
   while ( my $r = $maillog->next ) {
     next if $r->{text} =~ /AUTH|STARTTLS|--|NOQUEUE/;
 
@@ -139,6 +155,23 @@ sub run {
       $res->{$r->{id}}->{relay}->{to}     = $self->split_relay($r->{relay})
 	if exists $r->{relay};
 
+      $self->
+	sql_insert({
+		    table => 'rcpt_to',
+		    values =>
+		    [
+		     $r->{id} // undef,
+		     $r->{timestamp} // undef,
+		     $res->{$r->{id}}->{addr}->{to} // undef,
+		     $res->{$r->{id}}->{delay} // undef,
+		     $res->{$r->{id}}->{xdelay} // undef,
+		     $res->{$r->{id}}->{relay}->{to}->{ip} // undef,
+		     $res->{$r->{id}}->{relay}->{to}->{fqdn} // undef,
+		     $res->{$r->{id}}->{dsn} // undef,
+		     $res->{$r->{id}}->{status} // undef,
+		    ]
+		   });
+      
     } elsif ( exists $r->{'from'} ) {
 
       $res->{$r->{id}}->{timestamp}->{fr} = $r->{timestamp}         // 'NA';
@@ -149,6 +182,21 @@ sub run {
 	if exists $r->{msgid};
       $res->{$r->{id}}->{relay}->{fr}     = $self->split_relay($r->{relay})
 	if exists $r->{relay};
+
+      $self->
+	sql_insert({
+		    table => 'mail_from',
+		    values =>
+		    [
+		     $r->{id} // undef,
+		     $r->{timestamp} // undef,
+		     $res->{$r->{id}}->{addr}->{fr} // undef,
+		     $r->{size} // undef,
+		     $res->{$r->{id}}->{relay}->{fr}->{ip} // undef,
+		     $res->{$r->{id}}->{relay}->{fr}->{fqdn} // undef,
+		     $res->{$r->{id}}->{msgid} // undef,
+		    ]
+		   });
     }
 
     $res->{$r->{id}}->{connection} = $r->{status}
@@ -157,10 +205,12 @@ sub run {
       $r->{status} !~ /^.*did not issue.*$/;
   }
 
-  p $res if $self->o('export') eq 'raw' || $self->v > 2;
+  p $res if $self->o('export') eq 'raw' && $self->v > 2;
 
-  $self->tosqlite( { log_rows  => $res, } )
-    if $self->o('export') eq 'sqlite' && ! $self->o('dryrun');
+  if ($self->o('export') eq 'sqlite' && ! $self->o('dryrun')) {
+    $self->sql_db_create if ! exists $self->l->{db}->{name};
+    $self->tosqlite( { log_rows  => $res, } );
+  }
 
   exit 0;
 }
@@ -168,6 +218,272 @@ sub run {
 ######################################################################
 #
 ######################################################################
+
+sub sql_insert {
+  my ($self, $args) = @_;
+
+  my $dbh = DBI->connect("dbi:SQLite:dbname=" . $self->l->{db}->{name},"","",
+			 { AutoCommit => 1,
+			   RaiseError => 1, });
+
+  $dbh->do("PRAGMA cache_size = 100000") or die $dbh->errstr;
+  $dbh->begin_work or die $dbh->errstr;
+
+  my $q =   'INSERT OR IGNORE INTO ' . $args->{table} . ' VALUES (';
+  if ( $args->{table} eq 'rcpt_to' ) {
+    $q .= '?,?,?,?,?,?,?,?,?)';
+  } elsif ( $args->{table} eq 'mail_from' ) {
+    $q .= '?,?,?,?,?,?,?)';
+  }
+
+  my $sth = $dbh->prepare($q);
+  $sth->execute( @{$args->{values}} );
+
+  $dbh->commit or die $dbh->errstr;
+  $dbh->disconnect;
+}
+
+sub tosqlite {
+  my ($self, $args) = @_;
+
+  $args->{logfilemtime} = localtime($self->l->{stat}->mtime);
+
+  my $tl = localtime;
+  my $arg =
+    {
+     log_rows  => $args->{log_rows},
+    };
+
+  p $arg if $self->v > 3;
+  
+  print "database file to be used is: ",$self->l->{db}->{name},"\n" if $self->v > 1;
+
+  my $dbh = DBI->connect("dbi:SQLite:dbname=" . $self->l->{db}->{name},"","",
+			 { AutoCommit => 1,
+			   RaiseError => 1, });
+
+  $dbh->do("PRAGMA cache_size = 100000") or die $dbh->errstr;
+  $dbh->begin_work or die $dbh->errstr;
+
+  my $sth;
+  foreach ( keys ( %{$arg->{log_rows}} ) ) {
+    next if ! $arg->{log_rows}->{$_}->{addr}->{to} ||
+      ! $arg->{log_rows}->{$_}->{addr}->{fr};
+    $sth = $dbh->prepare('INSERT OR IGNORE INTO maillog VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+    $sth->execute( $_,
+		   $arg->{log_rows}->{$_}->{timestamp}->{to},
+		   $arg->{log_rows}->{$_}->{addr}->{fr},
+		   $arg->{log_rows}->{$_}->{addr}->{to},
+		   $arg->{log_rows}->{$_}->{size},
+		   $arg->{log_rows}->{$_}->{delay},
+		   $arg->{log_rows}->{$_}->{xdelay},
+		   $arg->{log_rows}->{$_}->{relay}->{fr}->{ip},
+		   $arg->{log_rows}->{$_}->{relay}->{fr}->{fqdn},
+		   $arg->{log_rows}->{$_}->{relay}->{to}->{ip},
+		   $arg->{log_rows}->{$_}->{relay}->{to}->{fqdn},
+		   $arg->{log_rows}->{$_}->{dsn},
+		   $arg->{log_rows}->{$_}->{msgid},
+		   $arg->{log_rows}->{$_}->{status},
+		 );
+  }
+
+  $dbh->commit or die $dbh->errstr;
+  $dbh->begin_work or die $dbh->errstr;
+
+  $arg->{addr_to_unique_select} = sprintf("
+INSERT INTO addr_to_unique (addr_to_unique, addr_to_count)
+       SELECT DISTINCT lower(addr_to), count(addr_to)
+              FROM maillog WHERE substr(addr_to,instr(addr_to, '\@') + 1) NOT IN ( '%s' )
+              AND addr_to NOT LIKE '%%,%%' GROUP BY addr_to",
+					  join("','", @{relay_domains()}, @{$self->o('relay_domains_sfx')}));
+
+  p $arg->{addr_to_unique_select} if $self->v > 1;
+  $dbh->do($arg->{addr_to_unique_select}) or die $dbh->errstr;
+  #$sth = $dbh->prepare( $arg->{addr_to_unique_select} );
+  #$sth->execute( "'" . join("','", @{relay_domains()}) . "','root'" );
+  $dbh->commit or die $dbh->errstr;
+
+  $dbh->disconnect;
+
+  $self->debug_msg({ priority  => 'info',
+	      message   => sprintf('info: processing complete %s%s -> %s',
+				  $self->l->{dirs},
+				  $self->l->{name},
+				  $self->l->{db}->{name}),
+	      verbosity => $self->v });
+}
+
+sub sql_db_create {
+  my ($self, $args) = @_;
+
+  $args->{logfilemtime} = localtime($self->l->{stat}->mtime);
+
+  my $tl = localtime;
+  my $arg =
+    {
+     localtime => $self->{_tl},
+     dbfile    =>
+     sprintf('%s%s-%s-v%s%s.sqlite',
+	     $self->l->{save_to} ne '' ? $self->l->{save_to} . '/' : $self->l->{dirs},
+	     $self->l->{name},
+	     $args->{logfilemtime}->ymd(''),
+	     $tl->ymd(''),
+	     $tl->hms(''),
+	    ),
+    };
+
+  $self->l->{db}->{name} = $arg->{dbfile};
+  p $arg if $self->v > 3;
+  print "database file to be created is: ",$self->l->{db}->{name},"\n" if $self->v > 1;
+
+  my $dbh = DBI->connect("dbi:SQLite:dbname=" . $self->l->{db}->{name},"","",
+			   { AutoCommit => 1,
+			     RaiseError => 1, });
+
+  $dbh->do("PRAGMA cache_size = 100000") or die $dbh->errstr;
+  $dbh->begin_work or die $dbh->errstr;
+
+  my $stub1 = $self->l->{logfile};
+  my $stub2 = $self->l->{db}->{name};
+  my $tbl_create = qq{CREATE TABLE maillog
+  -- $stub1 data processed, generated with mailstate
+  -- results are written to $stub2
+  (
+    id              TEXT PRIMARY KEY, -- sendmail message ID (macros \$i)
+    ts              TEXT,             -- timestamp
+    addr_fr         TEXT,             -- MAIL From
+    addr_to         TEXT,             -- RCPT To
+    size            NUM,              -- message size
+    delay           TEXT,             -- delay
+    xdelay          TEXT,             -- delay
+    relay_fr_ip     TEXT,             -- ip address of the sender relay
+    relay_fr_fqdn   TEXT,             -- fqdn of the sender relay
+    relay_to_ip     TEXT,             -- ip address of the recipient relay
+    relay_to_fqdn   TEXT,             -- fqdn of the recipient relay
+    dsn             TEXT,             -- DSN code
+    msgid           TEXT,             -- Message-ID header
+    stat            TEXT              -- Status
+  );};
+  p $tbl_create if $self->v > 1;
+  $dbh->do($tbl_create) or die $dbh->errstr;
+
+  $tbl_create = qq{CREATE TABLE IF NOT EXISTS addr_to_unique
+  -- $stub1 data processed, generated with mailstate
+  -- results are written to $stub2
+  -- table contains all unique, not served by us, RCPT TO addresses 
+  (
+    addr_to_unique TEXT PRIMARY KEY, -- RCPT TO (not ours recipients)
+    addr_to_count  NUM               -- emails sent to addr_to number
+  );};
+  p $tbl_create if $self->v > 1;
+  $dbh->do($tbl_create) or die $dbh->errstr;
+
+
+
+  $tbl_create = qq{CREATE TABLE rcpt_to
+  -- $stub1 data processed, generated with mailstate
+  -- results are written to $stub2
+  (
+    id              TEXT, -- sendmail message ID (macros \$i)
+    ts              TEXT,             -- timestamp
+    addr_to         TEXT,             -- RCPT To
+    delay           TEXT,             -- delay
+    xdelay          TEXT,             -- delay
+    relay_to_ip     TEXT,             -- ip address of the recipient relay
+    relay_to_fqdn   TEXT,             -- fqdn of the recipient relay
+    dsn             TEXT,             -- DSN code
+    stat            TEXT              -- Status
+  );};
+  p $tbl_create if $self->v > 1;
+  $dbh->do($tbl_create) or die $dbh->errstr;
+
+  $tbl_create = qq{CREATE TABLE mail_from
+  -- $stub1 data processed, generated with mailstate
+  -- results are written to $stub2
+  (
+    id              TEXT, -- sendmail message ID (macros \$i)
+    ts              TEXT,             -- timestamp
+    addr_fr         TEXT,             -- MAIL From 
+    size            NUM,              -- message size
+    relay_fr_ip     TEXT,             -- ip address of the sender relay
+    relay_fr_fqdn   TEXT,             -- fqdn of the sender relay
+    msgid           TEXT             -- Message-ID header
+  );};
+  p $tbl_create if $self->v > 1;
+  $dbh->do($tbl_create) or die $dbh->errstr;
+
+  my $idx_create =
+    [
+     q{CREATE INDEX m_addr_fr ON maillog ( addr_fr );},
+     q{CREATE INDEX m_addr_to ON maillog ( addr_to );},
+     q{CREATE INDEX m_from_to_addr ON maillog ( addr_fr, addr_to );},
+     q{CREATE INDEX m_relay_fr_ip ON maillog ( relay_fr_ip );},
+     q{CREATE INDEX m_relay_fr_fqdn ON maillog ( relay_fr_fqdn );},
+     q{CREATE INDEX m_relay_to_ip ON maillog ( relay_to_ip );},
+     q{CREATE INDEX m_relay_to_fqdn ON maillog ( relay_to_fqdn );},
+     q{CREATE INDEX m_msgid ON maillog ( msgid );},
+     q{CREATE INDEX m_stat ON maillog ( stat );},
+
+     q{CREATE INDEX f_addr_fr ON mail_from ( addr_fr );},
+     q{CREATE INDEX t_addr_to ON rcpt_to   ( addr_to );},
+     q{CREATE INDEX f_relay_fr_ip   ON mail_from ( relay_fr_ip );},
+     q{CREATE INDEX f_relay_fr_fqdn ON mail_from ( relay_fr_fqdn );},
+     q{CREATE INDEX t_relay_to_ip   ON rcpt_to ( relay_to_ip );},
+     q{CREATE INDEX t_relay_to_fqdn ON rcpt_to ( relay_to_fqdn );},
+    ];
+
+  foreach (@{$idx_create}) {
+    p $_ if $self->v > 1;
+    $dbh->do($_) or die $dbh->errstr;
+  }
+
+  $tbl_create = qq{CREATE VIEW view_maillog AS
+    SELECT f.id AS id,
+           f.ts AS ts,
+           f.addr_fr AS addr_fr,
+           t.addr_to AS addr_to,
+           f.size AS size,
+           t.delay AS delay,
+           t.xdelay AS xdelay,
+           f.relay_fr_ip AS relay_fr_ip,
+           f.relay_fr_fqdn AS relay_fr_fqdn,
+           t.relay_to_ip AS relay_to_ip,
+           t.relay_to_fqdn AS relay_to_fqdn,
+           t.dsn AS dsn,
+           f.msgid AS msgid,
+           t.stat AS stat
+    FROM mail_from AS f LEFT JOIN rcpt_to AS t ON f.id = t.id
+    ORDER BY f.ts, f.id;};
+  p $tbl_create if $self->v > 1;
+  $dbh->do($tbl_create) or die $dbh->errstr;
+
+  $dbh->commit or die $dbh->errstr;
+  $dbh->disconnect;
+}
+
+sub relay_domains {
+  my ($self, $args) = @_;
+  #   my $args = shift @_;
+#   my $relay_domains;
+#   my $ldap = Net::LDAP->new ( 'localhost', async => 1 ) or die "$@";
+#   my $bind_msg = $ldap->bind ( version => 3 );
+#   p $bind_msg->error if $bind_msg->code;
+#   my $fqdns =
+#     $ldap->search(base   => "sendmailMTAMapName=smarttable,ou=relay.xx,ou=Sendmail,dc=ibs",
+# 		  scope  => "one",
+# 		  filter => "sendmailMTAKey=*",
+# 		  attrs  => [ 'sendmailMTAKey' ],
+# 		 );
+#   my @fqdn_arr = $fqdns->entries;
+#   my $a;
+#   foreach ( @fqdn_arr ) {
+#     $a = $_->get_value('sendmailMTAKey', asref => 1);
+#     # p $a;
+#     push @{$relay_domains}, substr($a->[0],1);
+#   }
+#   return $relay_domains;
+  return [];
+}
 
 sub debug_msg {
   my ($self, $args) = @_;
@@ -209,156 +525,6 @@ sub strip_addr {
     $return = $addr;
   }
   return lc($return);
-}
-
-sub tosqlite {
-  my ($self, $args) = @_;
-
-  $args->{logfilemtime} = localtime($self->l->{stat}->mtime);
-
-  my $tl = localtime;
-  my $arg =
-    {
-     log_rows  => $args->{log_rows},
-     localtime => $self->{_tl},
-     dbfile    =>
-     sprintf('%s%s-%s-v%s%s.sqlite',
-	     $self->l->{save_to} ne '' ? $self->l->{save_to} . '/' : $self->l->{dirs},
-	     $self->l->{name},
-	     $args->{logfilemtime}->ymd(''),
-	     $tl->ymd(''),
-	     $tl->hms(''),
-	    ),
-    };
-
-  p $arg if $self->v > 3;
-  print "database file to be used is: $arg->{dbfile}\n" if $self->v;
-
-  my $dbh = DBI->connect("dbi:SQLite:dbname=$arg->{dbfile}","","",
-			 { AutoCommit => 1,
-			   RaiseError => 1, });
-
-  $dbh->do("PRAGMA cache_size = 100000") or die $dbh->errstr;
-  $dbh->begin_work or die $dbh->errstr;
-
-  my $tbl_create = qq{CREATE TABLE maillog
-  -- $self->l->{logfile} data processed, generated with mailstate
-  -- results are written to $arg->{dbfile}
-  (
-    id              TEXT PRIMARY KEY, -- sendmail message ID (macros \$i)
-    ts              TEXT,             -- timestamp
-    addr_fr         TEXT,             -- MAIL From
-    addr_to         TEXT,             -- RCPT To
-    size            NUM,              -- message size
-    delay           TEXT,             -- delay
-    xdelay          TEXT,             -- delay
-    relay_fr_ip     TEXT,             -- ip address of the sender relay
-    relay_fr_fqdn   TEXT,             -- fqdn of the sender relay
-    relay_to_ip     TEXT,             -- ip address of the recipient relay
-    relay_to_fqdn   TEXT,             -- fqdn of the recipient relay
-    dsn             TEXT,             -- DSN code
-    msgid           TEXT,             -- Message-ID header
-    stat            TEXT              -- Status
-  );};
-  p $tbl_create if $self->v > 1;
-  $dbh->do($tbl_create) or die $dbh->errstr;
-
-  my $idx_create = [ q{CREATE INDEX addr_fr ON maillog ( addr_fr );},
-		     q{CREATE INDEX addr_to ON maillog ( addr_to );},
-		     q{CREATE INDEX from_to_addr ON maillog ( addr_fr, addr_to );},
-		     q{CREATE INDEX relay_fr_ip ON maillog ( relay_fr_ip );},
-		     q{CREATE INDEX relay_fr_fqdn ON maillog ( relay_fr_fqdn );},
-		     q{CREATE INDEX relay_to_ip ON maillog ( relay_to_ip );},
-		     q{CREATE INDEX relay_to_fqdn ON maillog ( relay_to_fqdn );},
-		     q{CREATE INDEX msgid ON maillog ( msgid );},
-		     q{CREATE INDEX stat ON maillog ( stat );} ];
-
-  foreach (@{$idx_create}) {
-    p $_ if $self->v > 1;
-    $dbh->do($_) or die $dbh->errstr;
-  }
-
-  my $sth;
-  foreach ( keys ( %{$arg->{log_rows}} ) ) {
-    next if ! $arg->{log_rows}->{$_}->{addr}->{to} ||
-      ! $arg->{log_rows}->{$_}->{addr}->{fr};
-    $sth = $dbh->prepare('INSERT INTO maillog VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
-    $sth->execute( $_,
-		   $arg->{log_rows}->{$_}->{timestamp}->{to},
-		   $arg->{log_rows}->{$_}->{addr}->{fr},
-		   $arg->{log_rows}->{$_}->{addr}->{to},
-		   $arg->{log_rows}->{$_}->{size},
-		   $arg->{log_rows}->{$_}->{delay},
-		   $arg->{log_rows}->{$_}->{xdelay},
-		   $arg->{log_rows}->{$_}->{relay}->{fr}->{ip},
-		   $arg->{log_rows}->{$_}->{relay}->{fr}->{fqdn},
-		   $arg->{log_rows}->{$_}->{relay}->{to}->{ip},
-		   $arg->{log_rows}->{$_}->{relay}->{to}->{fqdn},
-		   $arg->{log_rows}->{$_}->{dsn},
-		   $arg->{log_rows}->{$_}->{msgid},
-		   $arg->{log_rows}->{$_}->{status},
-		 );
-  }
-
-  $dbh->commit or die $dbh->errstr;
-
-  $dbh->begin_work or die $dbh->errstr;
-  $tbl_create = qq{CREATE TABLE IF NOT EXISTS addr_to_unique
-  -- $self->l->{logfile} data processed, generated with mailstate
-  -- results are written to $arg->{dbfile}
-  -- table contains all unique, not served by us, RCPT TO addresses 
-  (
-    addr_to_unique TEXT PRIMARY KEY, -- RCPT TO (not ours recipients)
-    addr_to_count  NUM               -- emails sent to addr_to number
-  );};
-  p $tbl_create if $self->v > 1;
-  $dbh->do($tbl_create) or die $dbh->errstr;
-
-  $arg->{addr_to_unique_select} = sprintf("
-INSERT INTO addr_to_unique (addr_to_unique, addr_to_count)
-       SELECT DISTINCT lower(addr_to), count(addr_to)
-              FROM maillog WHERE substr(addr_to,instr(addr_to, '\@') + 1) NOT IN ( '%s' )
-              AND addr_to NOT LIKE '%%,%%' GROUP BY addr_to",
-					  join("','", @{relay_domains()}, @{$self->o('relay_domains_sfx')}));
-
-  p $arg->{addr_to_unique_select} if $self->v > 1;
-  $dbh->do($arg->{addr_to_unique_select}) or die $dbh->errstr;
-  #$sth = $dbh->prepare( $arg->{addr_to_unique_select} );
-  #$sth->execute( "'" . join("','", @{relay_domains()}) . "','root'" );
-  $dbh->commit or die $dbh->errstr;
-
-  $dbh->disconnect;
-
-  $self->debug_msg({ priority  => 'info',
-	      message   => sprintf('info: processing complete %s%s -> %s',
-				  $self->l->{dirs},
-				  $self->l->{name},
-				  $arg->{dbfile}),
-	      verbosity => $self->v });
-}
-
-sub relay_domains {
-  my ($self, $args) = @_;
-  #   my $args = shift @_;
-#   my $relay_domains;
-#   my $ldap = Net::LDAP->new ( 'localhost', async => 1 ) or die "$@";
-#   my $bind_msg = $ldap->bind ( version => 3 );
-#   p $bind_msg->error if $bind_msg->code;
-#   my $fqdns =
-#     $ldap->search(base   => "sendmailMTAMapName=smarttable,ou=relay.xx,ou=Sendmail,dc=ibs",
-# 		  scope  => "one",
-# 		  filter => "sendmailMTAKey=*",
-# 		  attrs  => [ 'sendmailMTAKey' ],
-# 		 );
-#   my @fqdn_arr = $fqdns->entries;
-#   my $a;
-#   foreach ( @fqdn_arr ) {
-#     $a = $_->get_value('sendmailMTAKey', asref => 1);
-#     # p $a;
-#     push @{$relay_domains}, substr($a->[0],1);
-#   }
-#   return $relay_domains;
-  return [];
 }
 
 1;
